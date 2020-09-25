72f06e3e6d2c9ce2a8a860592b24fdb9d9278d1a
Fabian Binz <fabian.binz@gmail.com>
2020-09-26 00:57:47 +0200

    Updates

---
+++ precache-manifest.4a3308c8fddddd98f2c156a32a0c3b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce131eef49ae6fe6a2643a9a3df193a",
+    "url": "/pdf-tools/index.html"
+  },
+  {
+    "revision": "26f4dc444cc920d81e87",
+    "url": "/pdf-tools/static/css/main.a79e2427.chunk.css"
+  },
+  {
+    "revision": "43023cf9088ab48509d5",
+    "url": "/pdf-tools/static/js/2.fbbb2a94.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/pdf-tools/static/js/2.fbbb2a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f4dc444cc920d81e87",
+    "url": "/pdf-tools/static/js/main.c081a362.chunk.js"
+  },
+  {
+    "revision": "ec35bd265983d3a5d39b",
+    "url": "/pdf-tools/static/js/runtime-main.b352d15c.js"
+  }
+]);
